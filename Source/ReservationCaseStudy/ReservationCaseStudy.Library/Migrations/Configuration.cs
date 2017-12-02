89012e95f6d831a31e7d95c10fa453f8bbccbce4
Shayane Tan <shayane_tan@yahoo.com>
2017-12-03 02:52:37 +0800

    Changed seeded data

--- Source/ReservationCaseStudy/ReservationCaseStudy.Library/Migrations/Configuration.cs
+++ Source/ReservationCaseStudy/ReservationCaseStudy.Library/Migrations/Configuration.cs
@@ -1,25 +1,25 @@
-namespace ReservationCaseStudy.Library.Migrations
+namespace ReservationCaseStudy.Migrations
 {
+    using ReservationCaseStudy.Library.Models;
     using System;
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ReservationCaseStudy.Library.ReservationDBContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ReservationCaseStudy.ReservationDBContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(ReservationCaseStudy.Library.ReservationDBContext context)
+        protected override void Seed(ReservationCaseStudy.ReservationDBContext context)
         {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-
             var stations = new List<Station>
             {
                 new Station{ Code="MLL", Description="Marshall Airport"},
